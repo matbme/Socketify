48cae5b882035b5000461961b13bfca47b4c45fc
Mateus Melchaides <mateusbmelchiades@gmail.com>
2020-09-26 21:40:23 -0300

    Created XCode project with Socket dependency

---
+++ PhoneApp/Socketify/Socketify/SocketifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketifyApp.swift
+//  Socketify
+//
+//  Created by Mateus Melchiades on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SocketifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
