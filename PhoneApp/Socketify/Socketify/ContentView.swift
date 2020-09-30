1dff9c0ed83a73a95363f7342ed3c1e2897956bb
Mateus Melchaides <mateusbmelchiades@gmail.com>
2020-09-30 19:17:17 -0300

    PlayPause control

--- PhoneApp/Socketify/Socketify/ContentView.swift
+++ PhoneApp/Socketify/Socketify/ContentView.swift
@@ -13,27 +13,69 @@ struct ContentView: View {
 	
 	@State private var client = TCPClient(address: "192.168.0.15", port: 8888)
 	@State private var nowPlaying: String = ""
+	@State private var playbackStatus: Image = Image(systemName: "icloud.slash")
+	@State private var artURL: String = ""
 	
     var body: some View {
 		VStack {
-			Spacer(minLength: 30)
+			getImageFromURL(forURL: artURL)
 			
 			ZStack {
-				Rectangle().fill(Color.red)
+				Rectangle()
+					.fill(Color.red)
+					.frame(height: 50)
 				
 				HStack {
-					Text("\(nowPlaying)").font(.headline)
+					Text("\(nowPlaying)")
+						.font(.callout)
+						.foregroundColor(.white)
+						.lineLimit(2)
 					
 					Spacer()
+					
+					HStack(alignment: .center, spacing: 20) {
+						Button {
+							makeRequest("prev", forClient: client)
+						} label: {
+							Image(systemName: "backward.end.fill")
+								.foregroundColor(.white)
+						}
+						
+						Button {
+							makeRequest("playpause", forClient: client)
+						} label: {
+							playbackStatus
+								.foregroundColor(.white)
+						}
+						
+						Button {
+							makeRequest("next", forClient: client)
+						} label: {
+							Image(systemName: "forward.end.fill")
+								.foregroundColor(.white)
+						}
+					}
 				}
 				.padding()
 			}
 		}
 		.onReceive(timer) { _ in
-			if let song = makeRequest("song", forClient: client) {
+			if let song = makeRequest("song", forClient: client) { //get current song
 				if let artist = makeRequest("artist", forClient: client) {
-					nowPlaying = artist.dropLast() + " - " + song.dropLast()
+					nowPlaying = artist.dropLast() + "\n" + song.dropLast()
 				}
+			}
+			
+			if let status = makeRequest("playbackstatus", forClient: client) {
+				if status.dropLast() == "▮▮" {
+					playbackStatus = Image(systemName: "play.fill")
+				} else {
+					playbackStatus = Image(systemName: "pause.fill")
+				}
+			}
+			
+			if let url = makeRequest("arturl", forClient: client) {
+				artURL = url.dropLast() + ""
 			}
 		}
     }
@@ -57,6 +99,35 @@ struct ContentView: View {
 		}
 		
 		return response
+	}
+	
+	func getImageFromURL(forURL: String) -> Image {
+		let replacedURL = forURL.dropLast().replacingOccurrences(of: "open.spotify.com", with: "i.scdn.co")
+		
+		guard let url = URL(string: replacedURL) else {
+			print("Invalid URL: \(replacedURL)")
+			return Image(systemName: "xmark.circle.fill")
+		}
+		
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			if let data = data {
+				print("data: ")
+				print(String(data: data, encoding: .utf8))
+				if let decodedResponse = try? JSONDecoder().decode(String.self, from: data) {
+					DispatchQueue.main.async {
+						print(decodedResponse)
+					}
+					return
+				}
+			} else {
+				print("No data")
+			}
+			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+		}.resume()
+		
+		return Image(systemName: "xmark.circle.fill")
 	}
 }
 
